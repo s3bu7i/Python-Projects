2b5885b49d98624a4baae0acd2818e76fcc192f8
Sabuhi Gasimzada <96132884+s3bu7i@users.noreply.github.com>
2022-08-12 16:21:07 +0400

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,23 @@
+
+
+
+
+class math:
+    def __init__(self,fnumber,snumber):
+        self.fnumber=fnumber
+        self.snumber=snumber
+        
+    def addition(self):
+        return self.fnumber+self.snumber
+    
+    def multiplication(self):
+        return self.fnumber*self.snumber
+    
+    def division(self):
+        return self.fnumber/self.snumber
+
+    def subtraction(self):
+        return self.fnumber-self.snumber
+    
+        
+        
