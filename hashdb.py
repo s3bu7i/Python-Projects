be6bae1dece092500b345fd87961ec097d94d6b3
Sabuhi Gasimzada <96132884+s3bu7i@users.noreply.github.com>
2022-08-17 01:17:35 +0400

    Add files via upload

---
+++ hashdb.py
@@ -0,0 +1,16 @@
+
+
+import hashlib
+
+veri=input("Enter the data to be encrypted : ")
+
+
+hashing=hashlib.sha256()
+hashing.update(veri.encode("utf-8"))
+
+#5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5
+print(hashing.hexdigest())
+
+
+
+
